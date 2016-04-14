f8d97e3b77639928b8de2f0d51d10fa71e84888f
Wiebo Troost <troostw@gmail.com>
2016-04-14 15:07:30 -0700

    add messaging on the login page to indicate progress and errors

--- src/binaryops/login.component.ts
+++ src/binaryops/login.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import {BinaryopsService} from  'angular2-binaryops/client';
 import {Router} from 'angular2/router';
+import {Alert} from 'ng2-bootstrap/ng2-bootstrap';
 
 // sidestep an error where module is not locally defined.
 var module: any = module || {id: 'dummy'};
@@ -11,7 +12,7 @@ var module: any = module || {id: 'dummy'};
   moduleId: module.id,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
+  directives: [Alert, FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 
 export class LoginComponent {
@@ -19,6 +20,8 @@ export class LoginComponent {
   password: string;
   _binaryopsService: BinaryopsService;
   _router: Router;
+  _loggingIn: boolean = false;
+  _loginErrorMsg : string = undefined;
   constructor(binaryopsService: BinaryopsService, router: Router) {
     this._binaryopsService = binaryopsService;
     this._router = router;
@@ -30,6 +33,8 @@ export class LoginComponent {
    */
   doLogin() {
     console.log('Going to login');
+    this._loggingIn = true;
+    this._loginErrorMsg = undefined;
     this._binaryopsService.login(this.username, this.password).subscribe(
         data => {
             console.log('Login OK');
@@ -37,9 +42,22 @@ export class LoginComponent {
           },
       err => {
         console.log('Login Error: ' + err);
+        this._loginErrorMsg = err;
+        this._loggingIn = false;
       },
-      () => console.log('Login complete')
+      () => {
+        console.log('Login complete');
+        this._loggingIn = false;
+      }
     );
     return;
+  }
+
+  noLoginInProgress() {
+    return !this._loggingIn;
+  }
+
+  noLoginError() {
+    return (this._loginErrorMsg === undefined);
   }
 }
